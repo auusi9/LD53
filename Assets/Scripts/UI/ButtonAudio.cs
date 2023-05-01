2534b9ab5b69d5837e02ea3a9c5e1e68efc1af07
auusi9 <ausi_dalmau@hotmail.com>
2023-05-02 01:37:56 +0200

    Audios

---
+++ Assets/Scripts/UI/ButtonAudio.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ButtonAudio : MonoBehaviour, IPointerEnterHandler
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private AudioSource _buttonAudioSource;
+        [SerializeField] private AudioClip _hoverButtonAudio;
+        [SerializeField] private AudioClip _pressButtonAudio;
+        private Vector2 _pitchRange = new Vector2(0.3f, 0.6f);
+        private float _initialVolume;
+
+        private void Start()
+        {
+            _initialVolume = _buttonAudioSource.volume;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if(_button.interactable)
+                PlayAudioClip(_hoverButtonAudio, 0.75f);
+        }
+
+        public void PlayClickSound()
+        {
+            PlayAudioClip(_pressButtonAudio, 1f);
+        }
+
+        private void PlayAudioClip(AudioClip audioClip, float volume)
+        {
+            _buttonAudioSource.clip = audioClip;
+            _buttonAudioSource.volume = _initialVolume * volume;
+            _buttonAudioSource.pitch = (Random.Range(_pitchRange.x, _pitchRange.y));
+            _buttonAudioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/MusicSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    public class MusicSingleton : MonoBehaviour
+    {
+        private MusicSingleton _musicSingleton;
+
+        private void Awake()
+        {
+            if (_musicSingleton == null)
+            {
+                _musicSingleton = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
